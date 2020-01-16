5f0fd3e82dd05e39b275c72c1f5c9f992086d1a0
Alexey Pogorelov <c_apogorelov@groupon.com>
2020-01-16 05:32:37 +0200

    Sh scripts added

---
+++ sh/exit_menu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while [ "$select" != "cancel" -a "$select" != "logout" -a "$select" != "poweroff" -a "$select" != "reboot"  ];
+do
+    select=$(echo -e 'cancel\nlogout\npoweroff\nreboot\nhibernate' | dmenu -nb '#1d2021' -nf '#928374' -sb '#d79921' -sf '#1d2021' -fn 'monospace-20' -i -l 180 -p "POWER")
+        [ -z "$select"  ] && exit 0
+      done
+      [ "$select" = "cancel"  ] && exit 0
+      [ "$select" = "logout"  ] && i3-msg exit
+      [ "$select" = "poweroff"  ] && sudo shutdown -h now 
+      [ "$select" = "reboot"  ] && sudo reboot
+      [ "$select" = "hibernate"  ] && sudo systemctl hibernate
+
---
+++ sh/search.sh
@@ -0,0 +1,2 @@
+ag --ignore 'node_modules' --ignore 'test' --ignore '**.map' --ignore '**.svg' --ignore '**.min.js' --ignore '**.test**' '$1'
+
