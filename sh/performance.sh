4a7f2d21501fc4f3c8a10812c2f448fa002e4db8
Alexey <c_alex@groupon.com>
2020-10-29 23:24:57 +0200

    feat: add mute / unmute notifications

--- sh/performance.sh
+++ sh/performance.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-while [ "$select" != "powersave" -a "$select" != "performance" ];
+while [ "$select" != "mute" -a "$select" != "unmute" -a "$select" != "powersave" -a "$select" != "performance" ];
 do
-    select=$(echo -e 'powersave\nperformance' | dmenu -nb '#1d2021' -nf '#928374' -sb '#d79921' -sf '#1d2021' -fn 'monospace-20' -i -l 180 -p "Performance")
+    select=$(echo -e 'mute\nunmute\npowersave\nperformance' | dmenu -nb '#1d2021' -nf '#928374' -sb '#d79921' -sf '#1d2021' -fn 'monospace-20' -i -l 180 -p "Performance")
         [ -z "$select"  ] && exit 0
       done
+      [ "$select" = "mute"  ] && killall -SIGUSR1 dunst
+      [ "$select" = "unmute"  ] && killall -SIGUSR2 dunst
       [ "$select" = "powersave"  ] && sudo cpupower frequency-set -g powersave
       [ "$select" = "performance"  ] && sudo cpupower frequency-set -g performance
 
