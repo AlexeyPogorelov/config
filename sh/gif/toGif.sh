b4233caa9d395b738cd154aade25c3aa752c9218
Oleksii Pohorielov <oleksii.pohorielov@dowjones.com>
2021-08-19 16:40:14 +0300

    compress gif after converting

--- sh/gif/toGif.sh
+++ sh/gif/toGif.sh
@@ -1,2 +1,3 @@
 ffmpeg -ss $1 -i ./test.mp4 -to $2 -f gif -filter_complex "[0:v] fps=12,scale=w=640:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -copyts ./test.gif
+# gifsicle -O3 --colors 256 --lossy=30 -o ./output.gif ./test.gif
 
